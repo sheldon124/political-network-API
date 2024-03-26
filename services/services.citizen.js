d23786f115732ba22af546db84eecbc6b6ef9e83
sheldon124 <sheldoncorrea98@gmail.com>
2024-03-26 14:44:44 -0400

    made change to check if user with passport already exists

--- services/services.citizen.js
+++ services/services.citizen.js
@@ -1,21 +1,21 @@
-const executePgQuery = require('../helpers/dbConnection');
+const executePgQuery = require("../helpers/dbConnection");
 
 const columnMap = {
-  firstName: 'first_name',
-  lastName: 'last_name',
-  dob: 'dob',
-  city: 'city',
-  passportNumber: 'passport_number',
-  contactNumber: 'phone',
-  email: 'email',
-  password: 'password',
-  role: 'role',
+  firstName: "first_name",
+  lastName: "last_name",
+  dob: "dob",
+  city: "city",
+  passportNumber: "passport_number",
+  contactNumber: "phone",
+  email: "email",
+  password: "password",
+  role: "role",
 };
 
 const registerCitizen = async (body) => {
   try {
-    let columns = '';
-    let values = '';
+    let columns = "";
+    let values = "";
 
     const checkEmailQuery = `SELECT * FROM citizen WHERE email='${body.email}';`;
 
@@ -23,15 +23,26 @@ const registerCitizen = async (body) => {
     console.log(checkEmailResp);
     if (checkEmailResp.rows?.length) {
       return {
-        message: 'user with email already exists',
+        message: "user with email already exists",
         status: 0,
+      };
+    }
+
+    const checkPassportQuery = `SELECT * FROM citizen WHERE passport_number='${body.email}';`;
+
+    const checkPassportResp = await executePgQuery(checkPassportQuery);
+    console.log(checkPassportResp);
+    if (checkEmailResp.rows?.length) {
+      return {
+        message: "user with passport already exists",
+        status: 2,
       };
     }
 
     Object.keys(body).forEach((key) => {
       if (body[key] !== undefined && columnMap[key]) {
         columns += `${columnMap[key]}, `;
-        values += `'${typeof body[key] === 'object' ? JSON.stringify(body[key]) : body[key]}', `;
+        values += `'${typeof body[key] === "object" ? JSON.stringify(body[key]) : body[key]}', `;
       }
     });
 
@@ -43,7 +54,7 @@ const registerCitizen = async (body) => {
     const response = await executePgQuery(query);
     console.log(response);
     return {
-      message: 'citizen added succesfully',
+      message: "citizen added succesfully",
       id: response.rows[0].citizen_id,
       status: 1,
     };
