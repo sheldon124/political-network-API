ab1e16e96b8065d93241ce354c1446e567006052
sheldon124 <sheldoncorrea98@gmail.com>
2024-03-27 19:55:23 -0400

    added routes for party

---
+++ routes/routes.party.js
@@ -0,0 +1,27 @@
+const { Router } = require("express");
+const {
+  getAlignedParties,
+  voteParty,
+  getAllParties,
+} = require("../services/services.party");
+
+const router = new Router();
+
+router.get("/", async (req, res) => {
+  const response = await getAllParties();
+  res.send(response);
+});
+
+router.post("/", async (req, res) => {
+  const { interests } = req.body;
+  const response = await getAlignedParties(interests);
+  res.send(response);
+});
+
+router.patch("/vote", async (req, res) => {
+  const { personId, partyId } = req.body;
+  const response = await voteParty(personId, partyId);
+  res.send(response);
+});
+
+module.exports = router;
---
+++ services/services.party.js
@@ -0,0 +1,92 @@
+const executePgQuery = require("../helpers/dbConnection");
+
+const getAllParties = async () => {
+  const query = `SELECT * FROM party;`;
+  try {
+    const res = await executePgQuery(query);
+    return {
+      result: res.rows,
+      status: 1,
+    };
+  } catch (error) {
+    return {
+      message: "error executing query",
+      status: 0,
+    };
+  }
+};
+
+const getAlignedParties = async (interests) => {
+  if (interests.length === 0)
+    return {
+      result: [],
+      status: 1,
+    };
+  try {
+    const query = `
+            SELECT "name" as party, interest AS interests
+            FROM party
+            WHERE EXISTS (
+                SELECT 1
+                FROM jsonb_array_elements(interest) AS i
+                WHERE ${interests.map((interest) => `i::text = '"${interest}"'`).join(" OR ")}
+            );`;
+
+    const res = await executePgQuery(query);
+
+    const partyList = res.rows.map((item) => {
+      const rowInterests = item.interests.filter((interest) =>
+        interests.includes(interest)
+      );
+      let percentMatch = (
+        (rowInterests.length / interests.length) *
+        100
+      ).toFixed(2);
+      return { ...item, interests: rowInterests, percentMatch };
+    });
+
+    return {
+      result: partyList,
+      status: 1,
+    };
+  } catch (error) {
+    return {
+      message: "error executing query",
+      status: 0,
+    };
+  }
+};
+
+const voteParty = async (personId, partyId) => {
+  try {
+    const voteExistsQuery = `SELECT id
+        FROM party
+        WHERE EXISTS (
+            SELECT 1
+            FROM jsonb_array_elements_text(party.vote) AS elem
+            WHERE elem::int = ${personId}
+        );`;
+    const existsResult = await executePgQuery(voteExistsQuery);
+    let voteQuery;
+    if (existsResult.rows.length) {
+      const currentVoteParty = existsResult.rows[0].id;
+      voteQuery = `SELECT vote_for_party(${partyId}, ${currentVoteParty}, ${personId});`;
+    } else {
+      voteQuery = ` UPDATE "party"
+      SET "vote" = "vote" || jsonb_build_array(${personId})
+      WHERE id = ${partyId};`;
+    }
+    await executePgQuery(voteQuery);
+    return {
+      result: "successfully voted",
+      status: 1,
+    };
+  } catch (error) {
+    return {
+      message: error.message,
+      status: 0,
+    };
+  }
+};
+
+module.exports = { getAlignedParties, voteParty, getAllParties };
