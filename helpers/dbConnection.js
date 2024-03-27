d0f3747dca5890d6da5786b0e1d3f4ec667f7110
sheldon124 <sheldoncorrea98@gmail.com>
2024-03-26 20:16:17 -0400

    fixed issue where pool was not defined

--- helpers/dbConnection.js
+++ helpers/dbConnection.js
@@ -1,12 +1,21 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 // const pool = new Pool({
-//   user: 'sheldonkevin',
-//   host: '127.0.0.1',
-//   database: 'citizenportal',
-//   password: '',
+//   user: "sheldonkevin",
+//   host: "127.0.0.1",
+//   database: "citizenportal",
+//   password: "",
 //   port: 5432,
 // });
+
+const pool = new Pool({
+  user: "ase_postgresql_db_user",
+  host: "cnu8u3sf7o1s7389g080-a.oregon-postgres.render.com",
+  database: "ase_postgresql_db",
+  password: "i9LaCUnEEJBM72OmSQekz8Tey8UM3xNv",
+  port: 5432,
+  ssl: true,
+});
 
 const executePgQuery = async (query, values) => {
   try {
